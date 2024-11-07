71dc3d028401d10756f281e06adf885d863ded34
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-07 22:59:36 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Personal Data
+Data Processing Script
 """
 
 import logging
@@ -15,15 +15,15 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ Replacing """
-    for f in fields:
-        message = re.sub(rf"{f}=(.*?)\{separator}",
-                         f'{f}={redaction}{separator}', message)
+    """ Redacts sensitive information in the log message. """
+    for fld in fields:
+        message = re.sub(rf"{fld}=(.*?)\{separator}",
+                         f'{fld}={redaction}{separator}', message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ RedactingFormatter class. """
+    """ Formatter class that redacts PII from log messages."""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -35,54 +35,53 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Format """
+        """ Initializes RedactingFormatter with specified PII fields. """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """ Implementing a logger.
+    """ Initializes and returns a logger instance.
     """
 
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(handler)
-    return logger
+    user_logger = logging.getLogger("user_data")
+    user_logger.setLevel(logging.INFO)
+    user_logger.propagate = False
+    user_handler = logging.StreamHandler()
+    user_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    user_logger.addHandler(user_handler)
+    return user_logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Implement db conectivity
+    """ Establishes a connection to the database.
     """
-    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
-    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(
-        host=host,
-        database=db_name,
-        user=username,
-        password=psw)
-    return conn
+    db_paswd = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    db_host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_nm = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conection = mysql.connector.connect(
+        host=db_host,
+        database=db_nm,
+        user=db_username,
+        password=db_paswd)
+    return conection
 
 
 def main() -> None:
-    """ Implement a main function
+    """ Main function that retrieves and prints user data from database.
     """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    for row in cursor:
-        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
-            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
-            f"last_login={row[6]}; user_agent={row[7]};"
+    datab = get_db()
+    db_cursor = datab.cursor()
+    db_cursor.execute("SELECT * FROM users;")
+    for us_rw in db_cursor:
+        message = f"name={us_rw[0]}; email={us_rw[1]}; phone={us_rw[2]}; " +\
+            f"ssn={us_rw[3]}; password={us_rw[4]};ip={us_rw[5]}; " +\
+            f"last_login={us_rw[6]}; user_agent={us_rw[7]};"
         print(message)
-    cursor.close()
-    db.close()
+    db_cursor.close()
+    datab.close()
 
 
 if __name__ == '__main__':
     main()
-  
