703fbf214478d5fc926e62848bb6b6624841a7d9
Kokiben <bennassarkaoutar@gmail.com>
2024-11-12 23:31:06 +0000

    index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,12 +1,48 @@
 #!/usr/bin/env python3
+""" Index View module
 """
-Route module for the API
-"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-from flask import Blueprint, abort
 
-index = Blueprint('index', __name__)
+@app_views.route('/status', strict_slashes=False)
+def fetch_status() -> str:
+    """ 
+    GET /api/v1/status
+    Response:
+      - Returns the status of the API.
+    """
+    return jsonify({"status": "OK"})
 
-@index.route('/api/v1/forbidden', methods=['GET'])
-def forbidden():
-    abort(403)  # This will trigger the 403 error handler in app.py
+
+@app_views.route('/stats/', strict_slashes=False)
+def fetch_statistics() -> str:
+    """ 
+    GET /api/v1/stats
+    Response:
+      - Returns the count of each object (e.g., users).
+    """
+    from models.user import User
+    object_counts = {}
+    object_counts['users'] = User.count()
+    return jsonify(object_counts)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def initiate_unauthorized() -> str:
+    """ 
+    GET /api/v1/unauthorized
+    Action:
+      - Aborts the request with a 401 Unauthorized error.
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def initiate_forbidden() -> str:
+    """ 
+    GET /api/v1/forbidden
+    Action:
+      - Aborts the request with a 403 Forbidden error.
+    """
+    abort(403)
