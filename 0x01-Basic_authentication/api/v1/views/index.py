89a84ae7f900aa0ba262d54e80f85a23a5f6df6e
Kokiben <bennassarkaoutar@gmail.com>
2024-11-14 11:06:50 -0800

    m

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python3
-""" Index View module
+""" Module that defines the index views and corresponding API endpoints.
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
-def fetch_status() -> str:
-    """
-    GET /api/v1/status
-    Response:
-      - Returns the status of the API.
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ Handles the GET request to /api/v1/status
+    Returns:
+      - A JSON response with the status of the API
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
-def fetch_statistics() -> str:
-    """
-    GET /api/v1/stats
-    Response:
-      - Returns the count of each object (e.g., users).
+def stats() -> str:
+    """ Handles the GET request to /api/v1/stats
+    Returns:
+      - A JSON response containing the count of various objects (e.g., users)
     """
     from models.user import User
-    object_counts = {}
-    object_counts['users'] = User.count()
-    return jsonify(object_counts)
+    statistics = {}
+    statistics['users'] = User.count()
+    return jsonify(statistics)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
-def initiate_unauthorized() -> str:
-    """
-    GET /api/v1/unauthorized
-    Action:
-      - Aborts the request with a 401 Unauthorized error.
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ Handles the GET request to /api/v1/unauthorized
+    Returns:
+      - A 401 Unauthorized error
     """
     abort(401)
 
 
-@app_views.route('/forbidden', strict_slashes=False)
-def initiate_forbidden() -> str:
-    """
-    GET /api/v1/forbidden
-    Action:
-      - Aborts the request with a 403 Forbidden error.
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Handles the GET request to /api/v1/forbidden
+    Returns:
+      - A 403 Forbidden error
     """
     abort(403)
