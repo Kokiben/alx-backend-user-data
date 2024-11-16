360840f00b47f4cb16b9d0c22446706a95cb0981
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-16 23:26:17 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for session authentication views.
+"""Module of session authenticating views.
 """
 import os
 from typing import Tuple
@@ -9,14 +9,13 @@ from models.user import User
 from api.v1.views import app_views
 
 
-@app_views.route(
-    '/auth_session/login', methods=['POST'], strict_slashes=False)
-def ses_ion_login() -> Tuple[str, int]:
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
     """POST /api/v1/auth_session/login
-    Returns:
-      - JSON representation of the authenticated User object.
+    Return:
+      - JSON representation of a User object.
     """
-    error_response = {"error": "no user found for this email"}
+    not_found_res = {"error": "no user found for this email"}
     email = request.form.get('email')
     if email is None or len(email.strip()) == 0:
         return jsonify({"error": "email missing"}), 400
@@ -24,28 +23,28 @@ def ses_ion_login() -> Tuple[str, int]:
     if password is None or len(password.strip()) == 0:
         return jsonify({"error": "password missing"}), 400
     try:
-        user_list = User.search({'email': email})
+        users = User.search({'email': email})
     except Exception:
-        return jsonify(error_response), 404
-    if len(user_list) <= 0:
-        return jsonify(error_response), 404
-    if user_list[0].is_valid_password(password):
+        return jsonify(not_found_res), 404
+    if len(users) <= 0:
+        return jsonify(not_found_res), 404
+    if users[0].is_valid_password(password):
         from api.v1.app import auth
-        session_id = auth.create_session(getattr(user_list[0], 'id'))
-        response = jsonify(user_list[0].to_json())
-        response.set_cookie(os.getenv("SESSION_NAME"), session_id)
-        return response
+        sessiond_id = auth.create_session(getattr(users[0], 'id'))
+        res = jsonify(users[0].to_json())
+        res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+        return res
     return jsonify({"error": "wrong password"}), 401
 
 @app_views.route(
     '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
-def ses_ion_logout() -> Tuple[str, int]:
+def logout() -> Tuple[str, int]:
     """DELETE /api/v1/auth_session/logout
-    Returns:
-      - An empty JSON object to confirm logout.
+    Return:
+      - An empty JSON object.
     """
     from api.v1.app import auth
-    session_destroyed = auth.destroy_session(request)
-    if not session_destroyed:
+    is_destroyed = auth.destroy_session(request)
+    if not is_destroyed:
         abort(404)
     return jsonify({})
