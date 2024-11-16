a67b941bf28e89eed421c2971558b15381118cf9
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-17 00:57:22 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""Session authentication module for the API.
+"""Session authentication module for managing API user sessions.
 """
 from uuid import uuid4
 from flask import request
-
 from .auth import Auth
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication class for managing user sessions.
+    """Handles session-based authentication for users.
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session ID for a given user.
+        """Creates a new session ID for the given user.
         """
         if isinstance(user_id, str):
             session_id = str(uuid4())
@@ -22,19 +21,19 @@ class SessionAuth(Auth):
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Gets the user ID for a given session ID.
+        """Retrieves the user ID associated with a given session ID.
         """
         if isinstance(session_id, str):
             return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> User:
-        """Retrieves the user associated with the current request.
+        """Retrieves the user object for the current request.
         """
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
 
     def destroy_session(self, request=None):
-        """Logs out the user by destroying the current session.
+        """Logs out the user by removing their session.
         """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
