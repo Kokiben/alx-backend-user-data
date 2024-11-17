9859efe7066207cc93f1df56fe5cfdf0c7f4dc8c
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-17 02:04:33 +0100

    Update session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Session authentication with expiration module for the API.
+"""Create a class SessionExpAuth
 """
 import os
 from flask import request
@@ -9,11 +9,11 @@ from .session_auth import SessionAuth
 
 
 class SessionExpAuth(SessionAuth):
-    """Session authentication class with expiration.
+    """Handles session-based authentication with expiration.
     """
 
     def __init__(self) -> None:
-        """Initializes a new SessionExpAuth instance.
+        """Initialize session expiration duration.
         """
         super().__init__()
         try:
@@ -22,7 +22,7 @@ class SessionExpAuth(SessionAuth):
             self.session_duration = 0
 
     def create_session(self, user_id=None):
-        """Creates a session id for the user.
+        """Create a session with expiration details.
         """
         session_id = super().create_session(user_id)
         if type(session_id) != str:
@@ -34,18 +34,17 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None) -> str:
-        """Retrieves the user id of the user associated with
-        a given session id.
+        """Get user ID for a valid session.
         """
         if session_id in self.user_id_by_session_id:
-            session_dict = self.user_id_by_session_id[session_id]
+            sessi_dct = self.user_id_by_session_id[session_id]
             if self.session_duration <= 0:
-                return session_dict['user_id']
-            if 'created_at' not in session_dict:
+                return sessi_dct['user_id']
+            if 'created_at' not in sessi_dct:
                 return None
-            cur_time = datetime.now()
-            time_span = timedelta(seconds=self.session_duration)
-            exp_time = session_dict['created_at'] + time_span
-            if exp_time < cur_time:
+            cr_tm = datetime.now()
+            tm_sp = timedelta(seconds=self.session_duration)
+            ex_tm = sessi_dct['created_at'] + tm_sp
+            if ex_tm < cr_tm:
                 return None
-            return session_dict['user_id']
+            return sessi_dct['user_id']
