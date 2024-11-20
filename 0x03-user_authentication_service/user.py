0de13a6d4cd9f9a8ef927d3186bf4a7e59606ce5
Kokiben <bennassarkaoutar@gmail.com>
2024-11-20 12:40:26 -0800

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """SQLAlchemy model for the 'users' table."""
     __tablename__ = 'users'
