17daaad4b8d9427a6895830712e618b20835ce38
Kokiben <bennassarkaoutar@gmail.com>
2024-11-20 13:56:28 -0800

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,31 +3,45 @@
 DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
-        Base.metadata.create_all(self._engine)  # Remove `drop_all` for persistence
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database"""
+        """Add a new user to the database
+
+        Args:
+            email (str): User's email address
+            hashed_password (str): Hashed password for the user
+
+        Returns:
+            User: The newly created User object
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
-        self._session.refresh(new_user)  # Ensures the object is up-to-date
         return new_user
